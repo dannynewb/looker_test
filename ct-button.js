c98b8a1a7dd415aa65e256fd6427236265f5a6ad
Daniel.Newby <Daniel.Newby@crispthinking.com>
2023-06-13 09:30:01 +0100

    test

---
+++ ct-button.js
@@ -0,0 +1,40 @@
+looker.plugins.visualizations.add({
+    // Id and Label are legacy properties that no longer have any function besides documenting
+    // what the visualization used to have. The properties are now set via the manifest
+    // form within the admin/visualizations page of Looker
+    id: "hello_world",
+    label: "Hello World",
+    options: {
+      font_size: {
+        type: "string",
+        label: "Font Size",
+        values: [
+          {"Large": "large"},
+          {"Small": "small"}
+        ],
+        display: "radio",
+        default: "large"
+      }
+    },
+    // Set up the initial state of the visualization
+    create: function(element, config) {
+  
+      // Insert a <style> tag with some styles we'll use later.
+      element.innerHTML = `
+      <ct-button
+      primary
+      class="save-btn"
+        Save
+    </ct-button>`;
+  
+    },
+    // Render in response to the data or settings changing
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+  
+      // Clear any errors from previous updates
+      this.clearErrors();
+  
+      // We are done rendering! Let Looker know.
+      done()
+    }
+  });
